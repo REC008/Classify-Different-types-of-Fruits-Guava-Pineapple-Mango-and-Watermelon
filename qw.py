1872e5cc337c54a3b7e5d2e8eff01116b296199e
Rohit Edgar Chauhan <74662897+REC008@users.noreply.github.com>
2024-04-16 15:02:15 +0530

    Add files via upload

---
+++ qw.py
@@ -0,0 +1,11 @@
+from simple_image_download import simple_image_download as sim
+
+my_downloader = sim.Downloader()
+
+my_downloader.directory = 'my_dir/'
+# Change File extension type
+my_downloader.extensions = '.jpg'
+print(my_downloader.extensions)
+
+my_downloader.download('skin', limit=3000)
+
---
+++ test.py
@@ -0,0 +1,81 @@
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import os
+
+data = []
+labels = []
+classes = 4
+cur_path = os.getcwd() #To get current directory
+
+
+classs = { 1:"Guava",
+    2:"mango",
+    3:"pineapple",
+    4:"watermelon"}
+
+i=1
+#Retrieving the images and their labels
+print("Obtaining Images & its Labels..............")
+for i in range(1,4):
+    path = os.path.join(cur_path,'C:\\Users\\rohit\\Desktop\\Intern K Proj\\AIprogK\\Fruits\\Dataset\\train\\',classs[i])
+    images = os.listdir(path)
+
+    for a in images:
+        try:
+            image = Image.open(path + '\\'+ a)
+            image = image.resize((30,30))
+            image = np.array(image)
+            data.append(image)
+            labels.append(i)
+            #print("{0} Loaded".format(a))
+        except:
+            print("Error loading image")
+print("Dataset Loaded")
+
+#Converting lists into numpy arrays
+data = np.array(data)
+labels = np.array(labels)
+
+print(data.shape, labels.shape)
+#Splitting training and testing dataset
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+#Converting the labels into one hot encoding
+y_train = to_categorical(y_train, 4)
+y_test = to_categorical(y_test, 4)
+
+def classify(img_file):
+    model = load_model('my_model.h5')
+    print("Loaded model from disk")
+    path2=img_file
+    print(path2)
+    test_image = Image.open(path2)
+    test_image = test_image.resize((30, 30))
+    test_image = np.expand_dims(test_image, axis=0)
+    test_image = np.array(test_image)
+    #result = model.predict_classes(test_image)[0]	
+    predict_x=model.predict(test_image)
+    result=np.argmax(predict_x,axis=1)
+    sign = classs[int(result) + 1]        
+    print(sign)
+
+
+import os
+path = 'Dataset\\test\\'
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+   for file in f:
+     if '.jpeg' in file:
+       files.append(os.path.join(r, file))
+
+for f in files:
+   classify(f)
+   print('\n')
